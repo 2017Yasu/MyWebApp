2e83995ed5784e912ed3dee85591319e59d3748a
2017Yasu <yasu.nkym.96@gmail.com>
2022-02-11 12:39:14 +0900

    create models for polls application

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    """
+    Question.
+
+    Attributes
+    ----------
+    question_text : CharField
+        Text of this question.
+    pub_date : DateTimeField
+        Date of publication.
+    """
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    """
+    Choice of a question.
+
+    Attributes
+    ----------
+    question : ForeignKey
+        Key to the corresponding question.
+    choice_text : CharField
+        Text of this choice.
+    votes : IntegerField
+        Number of votes for this choice.
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
