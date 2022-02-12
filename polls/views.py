c2895b6993a0c5680664d666145a5524c0bbe976
2017Yasu <yasu.nkym.96@gmail.com>
2022-02-12 13:27:22 +0900

    remove comment out

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from django.template import loader
 
 from .models import *
 
@@ -20,11 +19,9 @@ def index(request):
         Http response
     """
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    # return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
