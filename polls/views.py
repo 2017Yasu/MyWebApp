98788dc70f903d1e60386c94ab42a41b094ba91c
2017Yasu <yasu.nkym.96@gmail.com>
2022-02-12 13:45:43 +0900

    simplify the code for raising 404 error.

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponse
 
 from .models import *
@@ -40,11 +40,7 @@ def detail(request, question_id):
     HttpResponse
         Http response
     """
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exit")
-
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
