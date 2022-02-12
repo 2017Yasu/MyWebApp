8d56d1b884b126981dd7ec9748926737d7ae0fd1
2017Yasu <yasu.nkym.96@gmail.com>
2022-02-12 13:23:44 +0900

    change index view to display the list of questions.

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You are at the polls index")
+    """
+    Index page.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        Http request.
+
+    Returns
+    -------
+    HttpResponse
+        Http response
+    """
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     """
