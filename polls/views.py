d697ed19dae0bd586a00aeeee720b07754f38da0
2017Yasu <yasu.nkym.96@gmail.com>
2022-02-12 11:59:33 +0900

    add new views to polls app and register them.

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,58 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You are at the polls index")
+
+def detail(request, question_id):
+    """
+    View for displaying question text and vote form.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        Http request.
+    question_id : int
+        Question ID.
+
+    Returns
+    -------
+    HttpResponse
+        Http response
+    """
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    """
+    View for displaying the result of a specified question.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        Http request.
+    question_id : int
+        Question ID.
+
+    Returns
+    -------
+    HttpResponse
+        Http response
+    """
+    response = "You are looking at the result of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    """
+    View for voting on a specified question.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        Http request.
+    question_id : int
+        Question ID.
+
+    Returns
+    -------
+    HttpResponse
+        Http response
+    """
+    return HttpResponse("You are voting on question %s." % question_id)
